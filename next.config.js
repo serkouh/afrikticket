e3991ff6ad1ff9a2aa64146cb2174ba2b9fea50f
aymane-fast <aymanefast@gmail.com>
2024-12-20 20:11:34 +0100

    fix : deploying essue

--- next.config.js
+++ next.config.js
@@ -33,11 +33,13 @@ const nextConfig = {
 				port: '',
 				pathname: '/**',
 			},
+			{
+				protocol: 'https',
+				hostname: 'afrikticket.com',
+				port: '',
+				pathname: '/**',
+			  },
 		],
-		domains: ['afrikticket.com'],
-		deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
-		imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
-		formats: ['image/png', 'image/jpeg', 'image/jpg', 'image/webp'],
 	},
 }
 
